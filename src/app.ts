c4c19903a4abc02f7193d3c544b2bcbac5eebc04
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-02-18 23:59:13 +0530

    :zap: code for node's server

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import cors from "cors";
 import Debug from "debug";
+import path from "path";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import promClient from "prom-client";
 import { errorHandler, notFound } from "./middlewares";
 import { customMetric, jsonObjectMetric } from "./prometheusMetrics";
-const debug = Debug("server");
+import {
+  findNodeAddress,
+  getAllSeedServer,
+  getRandomSeedServer
+} from "./utils/peers";
+import { registerToSeedServer } from "./utils/register";
+import { IMessageBody } from "./interface";
+const debug = Debug("node:server");
 
-const initalizeApp = async (): Promise<express.Application> => {
+const initalizeApp = async (nodeId: string): Promise<express.Application> => {
   // Enable collection of default metrics
   promClient.collectDefaultMetrics();
-
   const app = express();
 
   // If we are behind some reverse proxy like Nginx then we can trust this
@@ -22,7 +29,36 @@ const initalizeApp = async (): Promise<express.Application> => {
   app.use(morgan("common"));
   app.use(express.json());
 
-  app.get("/", (req: express.Request, res: express.Response) => {
+  const allSeedServers = await getAllSeedServer(
+    path.join(__dirname, "seed.txt")
+  );
+
+  const randomSeedServer = getRandomSeedServer(allSeedServers);
+  debug("Seed server for registeration: ", randomSeedServer);
+
+  await registerToSeedServer({
+    seedServerAddress: randomSeedServer,
+    nodeId,
+    nodeAddress: `http://localhost:${process.env.PORT}`
+  });
+
+  app.post("/message", async (req: express.Request, res: express.Response) => {
+    const body = req.body as IMessageBody;
+    if (!body.message || !body.toNode) {
+      return res.status(400).send("Bad request");
+    }
+
+    debug("Request body: ", req.body);
+    const toNodeAddress = await findNodeAddress({
+      nodeId: body.toNode,
+      seedServer: randomSeedServer
+    });
+
+    debug("To Node address: ", toNodeAddress);
+    res.send(`OK: ${toNodeAddress}`);
+  });
+
+  app.get("/something", (req: express.Request, res: express.Response) => {
     // Increment the custom metric on each request
     customMetric.inc();
     // Simulate a JSON object as a metric value
@@ -47,7 +83,6 @@ const initalizeApp = async (): Promise<express.Application> => {
   app.use(notFound);
   app.use(errorHandler);
 
-  debug("App initalized....");
   return app;
 };
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import Debug from "debug";
 import initalizeApp from "./app";
-const debug = Debug("app");
+const debug = Debug("node");
 
 const startServer = async () => {
-  const app = await initalizeApp();
+  const app = await initalizeApp(process.env.NODE_ID);
 
   app.listen(parseInt(process.env.PORT, 10), () => {
     debug(
